3ed71da7ed544c6f29a339bd7cfa4f593dacafde
Aldiyahyaperkasa <aldiyahyap@gmail.com>
2022-04-18 16:14:59 +0800

    fix swipper card, navbar, contact and iframe map

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ var typed = new Typed(".typing", {
   backSpeed: 60,
   loop: true,
 });
+
+// swipper
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 25,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  loop: true,
+});
