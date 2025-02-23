431547a33a4f8dc35523aa000f042d0ad97afad4
aldiyahyaperkasa <aldiyahyap@gmail.com>
2025-02-23 23:28:08 +0800

    new portfolio

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing", {
-  strings: ["Mahasiswa IT", "Freelancer", "Front-End Junior"],
+  strings: ["Fresh Graduate", "Freelancer", "Web Developer"],
   typeSpeed: 100,
   backSpeed: 60,
   loop: true,
